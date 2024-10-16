d6d14a21fd67763b009ce5a298229755815e393d
Jens <34352934+jensmunkerud@users.noreply.github.com>
2024-10-16 20:15:51 +0200

    Initial Commit

---
+++ blueJoystickTests/blueJoystickTests.swift
@@ -0,0 +1,17 @@
+//
+//  blueJoystickTests.swift
+//  blueJoystickTests
+//
+//  Created by Jens Munkerud on 16/10/2024.
+//
+
+import Testing
+@testable import blueJoystick
+
+struct blueJoystickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
